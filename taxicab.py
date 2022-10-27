c34427a85114cc0919f6abaf033e6f79ebff3a32
klausmenendez <114318894+klausmenendez@users.noreply.github.com>
2022-10-26 23:23:04 -0700

    Update and rename main.py to taxicab.py

--- taxicab.py
+++ taxicab.py
@@ -10,27 +10,30 @@
 class TaxiCab:
 
     def __init__(self,x,y):
+        #define x and y coordinates
         self._x=x
         self._y=y
         self.reading=0
     def move_x(self, num):
-
+        #moves taxicab in the x direction
         self.num=num
         self.reading += abs(num)
         self._x = self._x+num
         return self._x
     def move_y(self, num2):
+        #moves taxicab in the y direction
         self.num2=num2
         self.reading += abs(num2)
         self._y = self._y+num2
         return self._y
     def odometer_reading(self):
+        #distance taxicab has goe
         new_x=self._x
         new_y=self._y
         print("At this point the cab has traveled",self.reading, "units and is now at coordinates (", new_x,new_y,")")
 
-cab=TaxiCab(5,-4)
-cab.move_x(-3)
-cab.move_y(5)
-cab.move_y(2)
-print(cab.odometer_reading())
+#cab=TaxiCab(5,-4)
+#cab.move_x(-3)
+#cab.move_y(5)
+#cab.move_y(2)
+#print(cab.odometer_reading())
